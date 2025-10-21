260a455e01502e0cafcb36447da8b5b9170df70a
Faisal Abid <ifaisalabid1@gmail.com>
2025-10-21 18:22:46 +0530

    Inherit from IdentityDbContext in Data

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 using ConnectLegal.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ConnectLegal.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<User>(options)
 {
     public DbSet<LawFirm> LawFirms => Set<LawFirm>();
     public DbSet<Lawyer> Lawyers => Set<Lawyer>();
@@ -34,10 +35,18 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
             e.Property(e => e.IsFeatured).HasDefaultValue(false);
             e.HasIndex(e => e.Name);
             e.HasIndex(e => e.IsFeatured);
+
             e.HasOne(l => l.LawFirm)
              .WithMany(f => f.Lawyers)
              .HasForeignKey(l => l.LawFirmId)
              .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        modelBuilder.Entity<User>(e =>
+        {
+            e.Property(u => u.FirstName).IsRequired().HasMaxLength(255);
+            e.Property(u => u.LastName).IsRequired().HasMaxLength(255);
+            e.Property(u => u.CreatedAt).IsRequired();
         });
     }
 }
